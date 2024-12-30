535a0d6087ff4d6bb1f2aa8bf937e7f76b14af82
Nestor M <ntor2004@gmail.com>
2024-12-30 13:32:07 +0100

    project created

---
+++ src/main/java/com/usafi/waste_management_system/WasteManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.usafi.waste_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WasteManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/usafi/waste_management_system/WasteManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usafi.waste_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
